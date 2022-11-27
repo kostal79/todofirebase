9097eb2aad55ff833048d0e234e4243fddc37612
Alex <74615724+kostal79@users.noreply.github.com>
2022-11-27 23:31:12 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,17 +7,6 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyB5LjV-EvG39YHyT5ZD6OykmVvdOtVPe4A",
-
-  authDomain: "todoapp-8c803.firebaseapp.com",
-
-  projectId: "todoapp-8c803",
-
-  storageBucket: "todoapp-8c803.appspot.com",
-
-  messagingSenderId: "557535992924",
-
-  appId: "1:557535992924:web:372851436c5256518a5a99",
 
 };
 
